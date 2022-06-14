e911ddaf1ed8e4df037dd45c99428409350e389d
Cezar Augusto Barboza Alves <cezaralves@sescms.com.br>
2022-06-14 15:20:52 -0400

    Up up

--- src/App.js
+++ src/App.js
@@ -1,12 +1,15 @@
-import logo from './logo.svg';
+import pizza from './assets/pizza.png';
 import './App.css';
+import CotacaoAtual from './components/CotacaoAtual';
 
 function App() {
+  
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
+        <CotacaoAtual />
+        <img src={pizza} className="App-logo" alt="logo" />
+        {/* <p>
           Edit <code>src/App.js</code> and save to reload.
         </p>
         <a
@@ -16,7 +19,7 @@ function App() {
           rel="noopener noreferrer"
         >
           Learn React
-        </a>
+        </a> */}
       </header>
     </div>
   );
---
+++ src/components/CotacaoAtual.js
@@ -0,0 +1,30 @@
+import { useEffect, useState } from "react";
+
+//const URL = process.env.API
+const URL = 'https://api.binance.com/api/v3/ticker/price?symbol=BTCUSDT'
+
+
+function CotacaoAtual(){
+    
+    const  [cotacao, setCotacao] = useState({})
+
+    useEffect(() => {
+        fetch(URL, {
+        }).then(function(response){
+            response.json()
+            .then(function (data){
+                setCotacao(data)
+               
+           })
+        }).catch(function (error){
+            console.log(error)
+        })
+    
+    }, [])
+            
+    return (
+        <h1>BTC/USDT:  $ {cotacao.price}</h1>
+    )    
+}
+
+export default CotacaoAtual;
